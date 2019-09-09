e1862aca94f15fc40568869f03e434be61f3f4f4
alexander <kucenkoaleksandr@gmail.com>
2019-09-10 00:07:25 +0300

    Added integration tests for UnitOfMeasureRepository

---
+++ src/test/java/ua/kaj/recipe/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,37 @@
+package ua.kaj.recipe.repositories;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import ua.kaj.recipe.domain.UnitOfMeasure;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UnitOfMeasureRepositoryIT {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void findByDescription() {
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+        assertEquals("Teaspoon", unitOfMeasureOptional.get().getDescription());
+    }
+
+    @Test
+    public void findByCup() {
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Cup");
+        assertEquals("Cup", unitOfMeasureOptional.get().getDescription());
+    }
+}
