ae20a7012003430026bca7a746dbb529cb3e54f8
alexander <kucenkoaleksandr@gmail.com>
2019-12-28 22:18:04 +0200

    adding create new recipe form and controller methods

--- src/main/java/ua/kaj/recipe/controllers/RecipeController.java
+++ src/main/java/ua/kaj/recipe/controllers/RecipeController.java
@@ -2,8 +2,11 @@ package ua.kaj.recipe.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ua.kaj.recipe.commands.RecipeCommand;
 import ua.kaj.recipe.services.datajpa.RecipeServiceImpl;
 
 @Controller
@@ -18,5 +21,18 @@ public class RecipeController {
     public String showById(@PathVariable String id, Model model) {
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
+    }
+
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand) {
+        RecipeCommand savedRecipeCommand = recipeService.saveRecipeCommand(recipeCommand);
+        return "redirect:/recipe/show/"+savedRecipeCommand.getId();
     }
 }
