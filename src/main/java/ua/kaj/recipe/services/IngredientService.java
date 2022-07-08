3272c54113f32ec01faa4fec3078b7ac2830f501
Alexander Kutsenko <kucenkoaleksandr@gmail.com>
2022-07-08 15:24:35 +0300

    adding annotation Service

--- src/main/java/ua/kaj/recipe/services/IngredientService.java
+++ src/main/java/ua/kaj/recipe/services/IngredientService.java
@@ -3,6 +3,7 @@ package ua.kaj.recipe.services;
 import org.springframework.stereotype.Service;
 import ua.kaj.recipe.domain.Ingredient;
 
+@Service
 public interface IngredientService extends CrudService<Ingredient>{
 
 }
